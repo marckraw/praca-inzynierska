f2becff76d97036bb958c5d85a34c4ee59109ad1
marckraw <marckraw@icloud.com>
2017-10-26 21:37:04 +0200

    [#115] add angular material, define custom material.module, make proxy - which doesn't work right now i guess

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { AddingProductComponent } from './adding-product/adding-product.component';
-import { ProductService } from './product.service';
+import { AddingProductComponent } from "./adding-product/adding-product.component";
+import { AppComponent } from "./app.component";
+import { MaterialModule } from "./material.module";
+import { ProductService } from "./product.service";
 
 @NgModule({
     declarations: [
         AppComponent,
-        AddingProductComponent
+        AddingProductComponent,
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         FormsModule,
         ReactiveFormsModule,
         HttpClientModule,
+        MaterialModule,
     ],
     providers: [ ProductService ],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
 })
 export class AppModule { }
