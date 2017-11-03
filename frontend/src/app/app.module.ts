22b0a4251863765c27c8d71f26935886d3eed194
marckraw <marckraw@icloud.com>
2017-11-03 19:43:53 +0100

    [#141] generate display-products component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AddingProductComponent } from "./adding-product/adding-product.componen
 import { AppComponent } from "./app.component";
 import { MaterialModule } from "./material.module";
 import { ProductService } from "./product.service";
+import { DisplayProductsComponent } from './display-products/display-products.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         AddingProductComponent,
+        DisplayProductsComponent,
     ],
     imports: [
         BrowserModule,
---
+++ frontend/src/app/display-products/display-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pi-display-products',
+  templateUrl: './display-products.component.html',
+  styleUrls: ['./display-products.component.scss']
+})
+export class DisplayProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
