f32c52e9ae18041614a273c923ca70399eef8672
marckraw <marckraw@icloud.com>
2017-12-02 15:44:59 +0100

    [NestJS] NestJS is working and is feeling fine, also in docker :)

--- backend/src/modules/app.module.ts
+++ backend/src/modules/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 
 @Module({
-  modules: [],
-  controllers: [AppController],
-  components: [],
+    modules: [],
+    controllers: [AppController],
+    components: [],
 })
 export class ApplicationModule {}
