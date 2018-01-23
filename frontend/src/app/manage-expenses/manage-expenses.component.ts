e806df6a8a97b5c5661d2e264809da8ee4f0444b
marckraw <marckraw@icloud.com>
2018-01-23 21:25:01 +0100

    Frontend side functionality for edit expenses

--- frontend/src/app/manage-expenses/manage-expenses.component.ts
+++ frontend/src/app/manage-expenses/manage-expenses.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Observable } from "rxjs/Observable";
 
+import { MatDialog } from "@angular/material";
+import { EditExpenseComponent } from "../edit-expense/edit-expense.component";
 import { IExpense } from "../models/expense.interface";
 import { ExpenseService } from "../services/expense.service";
 
@@ -12,7 +14,10 @@ import { ExpenseService } from "../services/expense.service";
 export class ManageExpensesComponent {
     public expenses: Observable<IExpense[]>;
 
-    constructor(private expenseService: ExpenseService) { }
+    constructor(
+        private expenseService: ExpenseService,
+        private dialog: MatDialog,
+    ) { }
 
     public ngOnInit() {
         this.showAllExpenses();
@@ -21,6 +26,27 @@ export class ManageExpensesComponent {
     public showAllExpenses() {
         this.expenseService.showExpenses().subscribe(val => console.log(val)); // this is only for loggin, temporary
         this.expenses = this.expenseService.showExpenses();
+    }
+
+    public edit(expense) {
+        const dialogRef = this.dialog.open(EditExpenseComponent, {
+            width: "600px",
+            data: { expense, confirmed: false },
+        });
+        dialogRef.afterClosed().subscribe(result => {
+            if (result !== undefined) {
+                if (result.confirmed) {
+                    console.log("teraz powinienem zaktualizowac dane");
+                    console.log("This is changed expense: ", result);
+                    // this.incomeService.addIncome(result.income).subscribe((data) => console.dir(data));
+                    // console.log("Dane gotowe do wysłania do końcówki, ", result.income);
+                } else {
+                    console.log("Nie potwierdziles danych. Popraw je...");
+                }
+            } else {
+                console.log("poszedles w pizdu");
+            }
+        });
     }
 
     public remove(expense) {
