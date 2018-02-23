6a36b0ba265eb9eee83227dec087cde2bf38dee7
marckraw <marckraw@icloud.com>
2018-02-23 22:52:46 +0100

    some fixes for adding, managing, and displaying expenses incomes products, tables

--- frontend/src/app/products/manage-products/manage-products.component.ts
+++ frontend/src/app/products/manage-products/manage-products.component.ts
@@ -8,49 +8,7 @@ import { EditProductComponent } from "./../edit-product/edit-product.component";
 
 @Component({
     selector: "pi-manage-products",
-    template:
-        `
-    <div class="manage-product-container">
-        <h1>Wszystkie produkty</h1>
-        <table *ngIf="(products | async) as products; else loading">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>index</th>
-                    <th>Name</th>
-                    <th>Company name</th>
-                    <th>Image</th>
-                    <th>Price</th>
-                    <th>Weight</th>
-                    <th>Category</th>
-                    <th>qType</th>
-                    <th>Tags</th>
-                    <th>&nbsp;</th>
-                    <th>&nbsp;</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr *ngFor="let product of products; let i = index">
-                    <td><strong>?</strong></td>
-                    <td><strong>{{ i }}</strong></td>
-                    <td>{{ product.productName }} </td>
-                    <td>{{ product.productCompanyName }}</td>
-                    <td>{{ product.productImage }}</td>
-                    <td>{{ product.productPrice }}</td>
-                    <td>{{ product.productWeight }}</td>
-                    <td>{{ product.productCategory }}</td>
-                    <td>{{ product.productQuantitativeType }}</td>
-                    <td>{{ product.productTags }}</td>
-                    <td><button mat-raised-button color="primary" (click)="edit(product)">EDIT</button></td>
-                    <td><button mat-raised-button color="primary" (click)="remove(product)">DELETE</button></td>
-                </tr>
-            </tbody>
-        </table>
-
-        <ng-template #loading>loading data...</ng-template>
-
-    </div>
-    `,
+    templateUrl: "manage-products.component.html",
     styleUrls: ["./manage-products.component.scss"],
 })
 export class ManageProductsComponent implements OnInit {
@@ -73,7 +31,6 @@ export class ManageProductsComponent implements OnInit {
 
     public edit(product) {
         const dialogRef = this.dialog.open(EditProductComponent, {
-            width: "600px",
             data: { product, confirmed: false },
         });
         dialogRef.afterClosed().subscribe(result => {
