528a748874f12cb26cd29d0c5823c745c359e5fe
marckraw <marckraw@icloud.com>
2018-02-26 23:31:45 +0100

    [76] add validator for password confirmation

--- frontend/src/app/sign-up/sign-up.component.ts
+++ frontend/src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators, ValidatorFn, ValidationErrors } from "@angular/forms";
 
 @Component({
     selector: "app-sign-up",
@@ -20,16 +20,66 @@ export class SignUpComponent {
             console.log("to są wartosci ktore przechodza frontendowa validacje");
             this.isErrorMsgVisible = false;
         } else {
+            console.log(this.formGroup);
             this.isErrorMsgVisible = true;
         }
+    }
+
+    public areEqual(): ValidatorFn {
+        return (group: FormGroup): ValidationErrors | null => {
+            const areEqual = Object.entries(group.value)
+                .map(value => value[1])
+                .reduce((acc: boolean, actualValue: string, index: number, array: string[]) => {
+                    acc = actualValue === array[index - 1];
+                    return acc;
+                }, false);
+
+            return areEqual
+                ? null
+                : {
+                      areEqual: {
+                          equal: false,
+                          errorMsg: "Hasła nie są takie same",
+                      },
+                  };
+        };
     }
 
     private createForm() {
         this.formGroup = this.formBuilder.group({
             login: ["", [Validators.required]],
             email: ["", [Validators.required]],
-            password: ["", [Validators.required]],
-            passwordRepeat: ["", [Validators.required]],
+            passwordGroup: this.formBuilder.group(
+                {
+                    password: ["", [Validators.required]],
+                    passwordConfirmation: ["", [Validators.required]],
+                },
+                { validator: this.areEqual() },
+            ),
         });
     }
 }
+
+
+
+// passwordGroup: this.formBuilder.group(
+//     {
+//         password: [
+//             '',
+//             [
+//                 this.validationService.isRequired(),
+//                 this.validationService.customMinLength(4),
+//                 this.validationService.customMaxLength(128),
+//             ],
+//         ],
+//         passConfirmation: [
+//             '',
+//             [
+//                 this.validationService.isRequired(),
+//                 this.validationService.customMinLength(4),
+//                 this.validationService.customMaxLength(128),
+//             ],
+//         ],
+//     },
+//     { validator: this.validationService.areEqual() },
+// ),
