45c18a7e3ecf88fdce2733164f6b4fd262ab4654
Marcin Krawczyk <marckraw@icloud.com>
2018-04-22 17:38:21 +0200

    We can select budget on budget page now and see charts related to the budget and expenses for budget Also, changed dashboard to be just board with links to the app pages

--- frontend/src/app/models/budget.interface.ts
+++ frontend/src/app/models/budget.interface.ts
@@ -1,11 +1,12 @@
 export interface ICategories {
-    id: number;
+    _id: string;
     name: string;
     declaredAmount: number;
     enteredAmount: number;
 }
 
 export interface IBudget {
+    _id: string;
     name: string;
     startDate: string;
     endDate: string;
